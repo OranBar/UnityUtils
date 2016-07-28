9fda0ce22481a419f137e45ef0a3205582922e83
Oran <baroran@hotmail.com>
2016-07-28 15:57:33 +0200

    Add files via upload
    EditorUpdate in OBMonoBehaviour

--- Assets/Scripts/OBMonoBehaviour.cs
+++ Assets/Scripts/OBMonoBehaviour.cs
@@ -1,13 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-[System.Obsolete("I don't see any reason to use this over Extension Methods, apart from overrides")]
 public class OBMonoBehaviour : MonoBehaviour {
 
-	public T GetOrAddComponent<T>() where T : Component{
-		T myComponent = base.GetComponent<T>();
-		return (myComponent!=null) ? myComponent : gameObject.AddComponent<T>();
+	public virtual void Update(){
+#if UNITY_EDITOR
+		if(UnityEditor.EditorApplication.isPlayingOrWillChangePlaymode == false){
+			EditorUpdate ();
+		}
+#endif
 	}
 
+	protected virtual void EditorUpdate(){
 
+	}
 }
