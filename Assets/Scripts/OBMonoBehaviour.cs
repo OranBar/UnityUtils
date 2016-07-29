ed7bacc8a9a97c29a24e24831ddd160e7d13ccb1
Oran <baroran@hotmail.com>
2016-07-29 10:32:47 +0200

    Add files via upload
    updates in moving and rotating coroutines.

--- Assets/Scripts/OBMonoBehaviour.cs
+++ Assets/Scripts/OBMonoBehaviour.cs
@@ -1,14 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[ExecuteInEditMode()]
 public class OBMonoBehaviour : MonoBehaviour {
 
-	public virtual void Update(){
+	public void Update(){
 #if UNITY_EDITOR
 		if(UnityEditor.EditorApplication.isPlayingOrWillChangePlaymode == false){
 			EditorUpdate ();
+		} else {
+			OnUpdate();
 		}
 #endif
+	}
+
+	protected virtual void OnUpdate(){
+		
 	}
 
 	protected virtual void EditorUpdate(){
