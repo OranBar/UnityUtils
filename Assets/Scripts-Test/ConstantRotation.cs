69d875c01f909547060caa2fef5a1858b80234a6
Oran <baroran@hotmail.com>
2016-07-31 12:46:43 +0200

    More work on holo effect

---
+++ Assets/Scripts-Test/ConstantRotation.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConstantRotation : MonoBehaviour {
+
+	public Vector3 anglePerSecond = new Vector3(0,0,20f);
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.Rotate(anglePerSecond * Time.deltaTime);
+	}
+}
