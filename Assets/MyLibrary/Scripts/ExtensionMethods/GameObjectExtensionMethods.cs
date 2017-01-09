f5650297c7c4c02c5783b8a3280a0caea2060a4e
Oran <baroran@hotmail.com>
2017-01-09 09:29:43 +0100

    Add files via upload
    ↓

--- Assets/MyLibrary/Scripts/ExtensionMethods/GameObjectExtensionMethods.cs
+++ Assets/MyLibrary/Scripts/ExtensionMethods/GameObjectExtensionMethods.cs
@@ -8,5 +8,16 @@ public static class GameObjectExtensionMethods {
 		return (myComponent!=null) ? myComponent : go.AddComponent<T>();
 	}
 
+    public static T AddOrGetComponent<T>(this GameObject go) where T : Component {
+        return go.GetOrAddComponent<T>();
+    }
 
+    public static MonoBehaviour GetMonoBehaviour(this GameObject go) {
+        if(go.GetComponent<MonoBehaviour>() == null) {
+            return go.AddComponent<DummyMonoBehaviour>();
+        } else {
+            return go.GetComponent<MonoBehaviour>();
+        }
+
+    }
 }
