83b94fa522ec6f418c504187d3a2c21c9f4724aa
Oran <baroran@hotmail.com>
2017-01-11 09:36:58 +0100

    Add files via upload
    added rectTranform extension method to compute position of UI element in screen pixels

---
+++ Assets/MyLibrary/Scripts/ExtensionMethods/RectTransformExtensionMethods.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace OranUnityUtils 
+{
+    public static class RectTransformExtensionMethods {
+        
+        public static Rect RectTransformToScreenSpace(this RectTransform transform) {
+            Vector2 size = Vector2.Scale(transform.rect.size, transform.lossyScale);
+            Rect rect = new Rect(transform.position.x, transform.position.y, size.x, size.y);
+            rect.x -= (transform.pivot.x * size.x);
+            rect.y -= (transform.pivot.y * size.y);
+            return rect;
+        }
+        public static Vector2 CenterToScreenSpace(this RectTransform rectTransf) {
+            return rectTransf.RectTransformToScreenSpace().center;
+        }
+    }
+}
